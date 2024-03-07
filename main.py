784b23b4e22294f13ba429024da9f62fc2c8e69a
denoqo <xq@utexas.edu>
2024-03-07 16:57:28 -0600

    add two engines: note_engine and query_engine

--- main.py
+++ main.py
@@ -1,11 +1,44 @@
 from dotenv import load_dotenv
 import os
 import pandas as pd
-
-
-population_path = os.path.join('data', 'population.csv')
-population = pd.read_csv(population_path)
+from llama_index.core.query_engine import PandasQueryEngine
+from prompts import new_prompt, instruction_str, context
+from note_engine import note_engine
+from llama_index.core.tools import QueryEngineTool, ToolMetadata
+from llama_index.core.agent import ReActAgent
+from llama_index.llms.openai import OpenAI
 
 load_dotenv()
 
-print(population.head())
+population_path = os.path.join('data', 'population.csv')
+population_df = pd.read_csv(population_path)
+
+
+population_query_engine = PandasQueryEngine(
+    df=population_df, verbose=True, instruction_str=instruction_str
+    )
+
+population_query_engine.update_prompts({"pandas_prompt": new_prompt})
+population_query_engine.query("What is the population of Canada?")
+
+
+tool_engines = [
+    note_engine,
+    QueryEngineTool(
+        population_query_engine,
+        metadata= ToolMetadata(
+            name="information",  
+            description="no information")
+    )
+]
+
+llm = OpenAI("gpt-3.5-turbo")
+agent = ReActAgent.from_tools(
+    tools= tool_engines,
+    llm=llm,
+    verbose=True
+)
+
+while (prompt := input("Enter a prompt (q to quit): ")) != "q":
+    results = agent.query(prompt)
+    print(results)
---
+++ note_engine.py
@@ -0,0 +1,20 @@
+from llama_index.core.tools import FunctionTool
+import os
+
+note_file = os.path.join('data', 'note.txt')
+
+def save_note(note):
+    if not os.path.exists(note_file):
+        open(note_file, 'w')
+
+    with open(note_file, 'a') as f:
+        f.writelines([note + '\n'])
+
+    return "note saved"
+
+
+note_engine = FunctionTool.from_defaults(
+    save_note, 
+    "note_saver", 
+    "Save a note to a file"
+)
